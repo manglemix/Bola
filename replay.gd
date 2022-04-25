96d75bbb2be45f2acbbde282d72b1a47e4453129
Manglemix <shabouza030@gmail.com>
2022-04-24 19:54:10 -0600

    improved replay selector

--- replay.gd
+++ replay.gd
@@ -22,7 +22,34 @@ func parse_replay(replay_name: String) -> int:
 	difficulty = data["difficulty"]
 	var date: Dictionary = data["date"]
 	
-	name = str(date["day"]) + "-" + str(date["month"]) + "-" + str(date["year"]).right(2) + "  " + str(date["hour"]) + ":" + str(date["minute"])
+	var minute_str: String
+	if date["minute"] < 10:
+		minute_str = "0" + str(date["minute"])
+	else:
+		minute_str = str(date["minute"])
+	
+	var is_pm := false
+	if date["hour"] > 12:
+		date["hour"] -= 12
+		is_pm = true
+	
+	name = str(date["day"]) + "-" + str(date["month"]) + "-" + str(date["year"]).right(2) + "  " + str(date["hour"]) + ":" + minute_str
+	
+	if is_pm:
+		name += " PM"
+	else:
+		name += " AM"
+	
+	name += "\n"
+	
+	if is_equal_approx(difficulty, GameState.GRANDMASTER):
+		name += "GRANDMASTER"
+	elif is_equal_approx(difficulty, GameState.PROFESSIONAL):
+		name += "PROFESSIONAL"
+	elif is_equal_approx(difficulty, GameState.AMATEUR):
+		name += "AMATEUR"
+	else:
+		name += "CASUAL"
 	
 	var img := Image.new()
 	err = img.load_png_from_buffer(data["thumbnail"])
